e97d8096b5bf72df1b34aef4be1a2bb1baf0f00d
Paul Mucur <mudge@mudge.name>
2020-05-04 14:10:34 +0100

    Run test steps inside action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,7 @@ apt-get install -yq --no-install-recommends ruby$1 ruby$1-dev
 
 wget -O libre2-dev.deb $2
 dpkg -i libre2-dev.deb
+
+gem install bundler -v '~> 1.11'
+bundle install --jobs=2 --retry=3 --deployment
+bundle exec rake
